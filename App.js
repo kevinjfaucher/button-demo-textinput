1d02b4aad3468050494389e238bd467b5a10cd74
Kevin <kfaucher@dtcc.edu>
2023-10-08 21:35:26 -0400

    Updating to class based syntax

--- App.js
+++ App.js
@@ -1,90 +1,95 @@
-// Start by importing necessary components and hooks from React and React Native.
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 
-// This is our main App component.
-export default function App() {
-  // Here, we set up state variables. Think of them as variables whose changes can be 'watched' by React.
-  const [name, setName] = useState('');
-  const [favoriteColor, setFavoriteColor] = useState('');
-  const [message, setMessage] = useState('');
-
-  // This function updates the message based on which greeting button is pressed.
-  const handleButtonPress = (option) => {
-    if (option === 1) {
-      setMessage(`Hello, ${name}! You must like the color ${favoriteColor}.`);
-    } else if (option === 2) {
-      setMessage(`Did you know, ${name}, that ${favoriteColor} is popular this season?`);
-    } else if (option === 3) {
-      setMessage(`${name}, wearing ${favoriteColor} today?`);
-    } else {
-      setMessage('');
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            favoriteColor: '',
+            message: '',
+        };
     }
-  };
 
+    handleButtonPress(option) {
+        const { name, favoriteColor } = this.state;
+        switch (option) {
+            case 1:
+                this.setState({ message: `Hello, ${name}! You must like the color ${favoriteColor}.` });
+                break;
+            case 2:
+                this.setState({ message: `Did you know, ${name}, that ${favoriteColor} is a popular color this season?` });
+                break;
+            case 3:
+                this.setState({ message: `${name}, wearing ${favoriteColor} today?` });
+                break;
+            default:
+                this.setState({ message: '' });
+        }
+    }
 
-  // This is what our app renders (displays).
-  return (
-      <View style={styles.container}>
-        {/* Input for name */}
-        <Text style={styles.label}>What's your name?</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Enter your name"
-            // Save the text entered by the user in the 'name' state.
-            onChangeText={text => setName(text)}
-        />
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.label}>What's your name?</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your name"
+                    onChangeText={(text) => this.setState({ name: text })}
+                />
 
-        {/* Input for favorite color */}
-        <Text style={styles.label}>What's your favorite color?</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Enter your favorite color"
-            // Save the text entered by the user in the 'favoriteColor' state.
-            onChangeText={text => setFavoriteColor(text)}
-        />
+                <Text style={styles.label}>What's your favorite color?</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your favorite color"
+                    onChangeText={(text) => this.setState({ favoriteColor: text })}
+                />
 
-        {/* Three buttons that each generate a different greeting */}
-        <Button title="Greeting #1" onPress={() => handleButtonPress(1)} />
-        <Button title="Greeting #2" onPress={() => handleButtonPress(2)} style={styles.buttonSpacing} />
-        <Button title="Greeting #3" onPress={() => handleButtonPress(3)} style={styles.buttonSpacing} />
+                <Button
+                    title="Greeting #1"
+                    onPress={() => this.handleButtonPress(1)}
+                />
+                <Button
+                    title="Greeting #2"
+                    onPress={() => this.handleButtonPress(2)}
+                    style={styles.buttonSpacing}
+                />
+                <Button
+                    title="Greeting #3"
+                    onPress={() => this.handleButtonPress(3)}
+                    style={styles.buttonSpacing}
+                />
 
-        {/* Display the generated greeting message */}
-        <Text style={styles.messageText}>{message}</Text>
-      </View>
-  );
+                <Text style={styles.messageText}>{this.state.message}</Text>
+            </View>
+        );
+    }
 }
 
-// These are styles for our components, which make them look nice!
 const styles = StyleSheet.create({
-  // Style for the main container/view
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-  },
-  // Style for the labels (e.g., "What's your name?")
-  label: {
-    marginTop: 15,
-    marginBottom: 5,
-    fontWeight: 'bold',
-  },
-  // Style for the text input boxes
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingLeft: 10,
-    marginBottom: 20,
-  },
-  // Space between buttons
-  buttonSpacing: {
-    marginVertical: 10,
-  },
-  // Style for the greeting message
-  messageText: {
-    marginTop: 20,
-    textAlign: 'center',
-    fontSize: 16,
-  },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    label: {
+        marginTop: 15,
+        marginBottom: 5,
+        fontWeight: 'bold',
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingLeft: 10,
+        marginBottom: 20,
+    },
+    buttonSpacing: {
+        marginVertical: 10,
+    },
+    messageText: {
+        marginTop: 20,
+        textAlign: 'center',
+        fontSize: 16,
+    },
 });
